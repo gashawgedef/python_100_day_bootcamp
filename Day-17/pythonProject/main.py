77c841a04866d620079680ab950b57c5637ccc99
gashawgedef@gmail.com <gashawgedef@gmail.com>
2024-04-24 22:33:31 +0300

    day 17 final

--- Day-17/pythonProject/main.py
+++ Day-17/pythonProject/main.py
@@ -2,6 +2,7 @@
 
 from question_model import Question
 from data import question_data
+from quiz_brain import  QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -9,6 +10,14 @@ for question in question_data:
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+#
+# print(question_bank[1].text,question_bank[1].answer)
+#
+# for index, q_bank in enumerate(question_bank):
+#     print(f"{index+1}. {q_bank.text}",q_bank.answer)
 
-print(question_bank[0].text)
-
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+print("You have completed the quiz")
+print(f"Your Final Score is {quiz.score}/{quiz.question_number}")
--- Day-17/pythonProject/question_model.py
+++ Day-17/pythonProject/question_model.py
@@ -1,10 +1,10 @@
 
 
 class Question:
-    def __init__(self,text,answer):
-        self.text = text
-        self.answer = answer
+    def __init__(self,q_text,q_answer):
+        self.text = q_text
+        self.answer = q_answer
 
 
-new_question = Question("What is your name?","False")
-print(new_question.text)
+# new_question = Question("What is your name?","False")
+# print(new_question.text)
